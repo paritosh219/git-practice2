81cbc32b620573c9871545ac3d51e3975190bc64
Paritosh <paritoshsaini3@gmail.com>
2025-05-12 11:50:12 +0530

    Initial commit

---
+++ src/main/java/com/capgemini/githubproject/GithubprojectApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.githubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubprojectApplication.class, args);
+    }
+
+}
